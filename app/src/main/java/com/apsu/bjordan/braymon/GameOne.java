548e89384a5dc0f919d421224601a8e810c65845
Todd <Toddab16@gmail.com>
2017-02-23 13:29:03 -0500

    Sounds added and attached to CPU turn

--- app/src/main/java/com/apsu/bjordan/braymon/GameOne.java
+++ app/src/main/java/com/apsu/bjordan/braymon/GameOne.java
@@ -3,31 +3,37 @@ package com.apsu.bjordan.braymon;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.TransitionDrawable;
+import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
-
+import java.util.Set;
 
 
 public class GameOne extends AppCompatActivity {
 
+    private SoundPool soundPool;
+    private Set<Integer> soundsLoaded;
+
+    int blue_sound;
+    int red_sound;
+    int green_sound;
+    int yellow_sound;
+
     ArrayList<Integer> cpu = new ArrayList<Integer>();
     ArrayList<Integer> player = new ArrayList<Integer>();
-
-    MediaPlayer mp_blue;
-    MediaPlayer mp_green;
-    MediaPlayer mp_red;
-    MediaPlayer mp_yellow;
-    MediaPlayer mp_fail;
 
     private UpdateTask sg;
     int it = 0;
@@ -36,12 +42,6 @@ public class GameOne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_i);
-
-        mp_blue = MediaPlayer.create(getApplicationContext(), R.raw.blue_note);
-        mp_green = MediaPlayer.create(getApplicationContext(), R.raw.green_note);
-        mp_red = MediaPlayer.create(getApplicationContext(), R.raw.red_note);
-        mp_yellow = MediaPlayer.create(getApplicationContext(), R.raw.yellow_note);
-        mp_fail = MediaPlayer.create(getApplicationContext(), R.raw.fail_note);
 
         Button start = (Button) findViewById(R.id.start_button);
         start.setOnClickListener(new View.OnClickListener() {
@@ -52,50 +52,86 @@ public class GameOne extends AppCompatActivity {
             }
         });
 
-            ImageButton blue = (ImageButton) findViewById(R.id.imageButton_Blue);
-            blue.setOnClickListener(new View.OnClickListener() {
+        ImageButton blue = (ImageButton) findViewById(R.id.imageButton_Blue);
+        blue.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                selectButton(1);
+                player.add(1);
+                checkStatus();
+            }
+        });
+
+        ImageButton red = (ImageButton) findViewById(R.id.imageButton_Red);
+        red.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                selectButton(2);
+                player.add(2);
+                checkStatus();
+            }
+        });
+
+        ImageButton green = (ImageButton) findViewById(R.id.imageButton_Green);
+        green.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                selectButton(3);
+                player.add(3);
+                checkStatus();
+            }
+        });
+
+        ImageButton yellow = (ImageButton) findViewById(R.id.imageButton_Yellow);
+        yellow.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                selectButton(4);
+                player.add(4);
+                checkStatus();
+            }
+        });
+
+        soundsLoaded = new HashSet<Integer>();
+
+    }
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                soundPool = new SoundPool.Builder().setMaxStreams(10).build();
+            }
+            else {
+                soundPool = new SoundPool(10,AudioManager.STREAM_MUSIC, 1);
+            }
+
+            soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener() {
                 @Override
-                public void onClick(View view) {
-                    selectButton(1);
-                    player.add(1);
-                    checkStatus();
+                public void onLoadComplete(SoundPool soundPool, int sampleId, int status) {
+                    if (status == 0 ) {
+                        soundsLoaded.add(sampleId);
+                    } else {
+                        Log.i("SOUND", "Sound Error");
+                    }
                 }
             });
 
-            ImageButton red = (ImageButton) findViewById(R.id.imageButton_Red);
-            red.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View view) {
-                    selectButton(2);
-                    player.add(2);
-                    checkStatus();
-                }
-            });
-
-            ImageButton green = (ImageButton) findViewById(R.id.imageButton_Green);
-            green.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View view) {
-                    selectButton(3);
-                    player.add(3);
-                    checkStatus();
-                }
-            });
-
-            ImageButton yellow = (ImageButton) findViewById(R.id.imageButton_Yellow);
-            yellow.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View view) {
-                    selectButton(4);
-                    player.add(4);
-                    checkStatus();
-                }
-            });
+            blue_sound = soundPool.load(this, R.raw.blue_note, 1);
+            red_sound = soundPool.load(this, R.raw.red_note, 1);
+            green_sound = soundPool.load(this, R.raw.green_note, 1);
+            yellow_sound = soundPool.load(this, R.raw.yellow_note, 1);
         }
+
+
+
+
+
 
     public void checkStatus () {
         if (player.get(it) == cpu.get(it)) {
@@ -182,9 +218,10 @@ public class GameOne extends AppCompatActivity {
 
         b.setImageDrawable(td);
 
-        mp_blue.start();
-        td.startTransition(1000);
-        td.reverseTransition(1000);
+        playSound(blue_sound);
+        td.startTransition(500);
+        td.reverseTransition(500);
+
     }
 
     public void lightRed () {
@@ -198,10 +235,10 @@ public class GameOne extends AppCompatActivity {
         b.setImageDrawable(td);
 
 
-        mp_red.start();
-        td.startTransition(1000);
-        td.reverseTransition(1000);
-    }
+        playSound(red_sound);
+        td.startTransition(500);
+        td.reverseTransition(500);
+     }
 
     public void lightGreen () {
         ImageButton b = (ImageButton) findViewById(R.id.imageButton_Green);
@@ -213,9 +250,9 @@ public class GameOne extends AppCompatActivity {
 
         b.setImageDrawable(td);
 
-        mp_green.start();
-        td.startTransition(1000);
-        td.reverseTransition(1000);
+        playSound(green_sound);
+        td.startTransition(500);
+        td.reverseTransition(500);
     }
 
     public void lightYellow () {
@@ -229,9 +266,15 @@ public class GameOne extends AppCompatActivity {
         b.setImageDrawable(td);
 
 
-        mp_yellow.start();
-        td.startTransition(1000);
-        td.reverseTransition(1000);
+        playSound(yellow_sound);
+        td.startTransition(500);
+        td.reverseTransition(500);
+    }
+
+    private void playSound(int soundId) {
+        if (soundsLoaded.contains(soundId)) {
+            soundPool.play(soundId, 1.0f, 1.0f, 0, 0, 2.0f);
+        }
     }
 
 
